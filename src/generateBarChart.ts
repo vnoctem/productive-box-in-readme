d48c9deacd161ef6fffe09b6a8d3249ec6afab1b
Vincent <vincent.ngo.gauthier@gmail.com>
2021-12-08 21:57:41 -0500

    Update generateBarChart.ts

--- src/generateBarChart.ts
+++ src/generateBarChart.ts
@@ -4,14 +4,14 @@
 export default function generateBarChart(percent: number, size: number) {
   const syms = '░▏▎▍▌▋▊▉█';
 
-  const frac = Math.floor((size * 6 * percent) / 100);
-  const barsFull = Math.floor(frac / 6);
+  const frac = Math.floor((size * 8 * percent) / 100);
+  const barsFull = Math.floor(frac / 8);
   if (barsFull >= size) {
-    return syms.substring(6, 7).repeat(size);
+    return syms.substring(8, 9).repeat(size);
   }
-  const semi = frac % 6;
+  const semi = frac % 8;
 
-  return [syms.substring(6, 7).repeat(barsFull), syms.substring(semi, semi + 1)]
+  return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
     .join('')
     .padEnd(size, syms.substring(0, 1));
 }
