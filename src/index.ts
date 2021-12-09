227ad396abe687fa2f716ff24996e176df324646
Vincent <vincent.ngo.gauthier@gmail.com>
2021-12-08 22:03:26 -0500

    Decrease barchart length

--- src/index.ts
+++ src/index.ts
@@ -84,7 +84,7 @@ interface IRepo {
     const line = [
       `${cur.label}`.padEnd(10),
       `${cur.commits.toString().padStart(5)} commits`.padEnd(14),
-      generateBarChart(percent, 21),
+      generateBarChart(percent, 17),
       String(percent.toFixed(1)).padStart(5) + '%',
     ];
 
