2db0c61de8d76c0ca12ca9a1575979d6fc3c566f
Vincent <vincent.ngo.gauthier@gmail.com>
2022-01-30 00:25:11 -0500

    Try different blocks to graph

--- src/generateBarChart.ts
+++ src/generateBarChart.ts
@@ -2,16 +2,16 @@
  * clone from https://github.com/matchai/waka-box
  */
 export default function generateBarChart(percent: number, size: number) {
-  const syms = '░▏▎▍▌▋▊▉█';
+  const syms = '░▒▓█';
 
-  const frac = Math.floor((size * 8 * percent) / 100);
-  const barsFull = Math.floor(frac / 8);
+  const frac = Math.floor((size * 3 * percent) / 100);
+  const barsFull = Math.floor(frac / 3);
   if (barsFull >= size) {
-    return syms.substring(8, 9).repeat(size);
+    return syms.substring(3, 4).repeat(size);
   }
-  const semi = frac % 8;
+  const semi = frac % 3;
 
-  return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
+  return [syms.substring(3, 4).repeat(barsFull), syms.substring(semi, semi + 1)]
     .join('')
     .padEnd(size, syms.substring(0, 1));
 }
